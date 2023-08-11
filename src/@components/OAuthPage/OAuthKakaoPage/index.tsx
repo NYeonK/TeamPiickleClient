ae1df9401461a661cbaaf654ce907f1ee8dc6f7e
NYeonK <skdus728@gmail.com>
2023-08-11 14:59:12 +0900

    [ etc ] alert창 제거

--- src/@components/OAuthPage/OAuthKakaoPage/index.tsx
+++ src/@components/OAuthPage/OAuthKakaoPage/index.tsx
@@ -13,44 +13,35 @@ export default function OAuthKakaoPage() {
 
   const { login } = useAuth();
   const loginWithUserToken = (accessToken: string) => {
-    alert("loginWithUserToken()입니다!");
-
     login(accessToken);
     navigate(routePaths.Main);
   };
 
   const getKakaoToken = async (authorizationCode: string) => {
-    try {
-      const response = await axios.post(
-        "https://kauth.kakao.com/oauth/token",
-        qs.stringify({
-          grant_type: "authorization_code",
-          client_id: import.meta.env.VITE_KAKAO_CLIENT_ID,
-          redirect_uri: import.meta.env.DEV
-            ? "http://127.0.0.1:5173/oauth/kakao"
-            : import.meta.env.VITE_KAKAO_REDIRECT_URI,
-          code: authorizationCode,
-          client_secret: import.meta.env.VITE_KAKAO_CLIENT_SECRET,
-        }),
-        {
-          headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" },
-        },
-      );
-      return response.data.access_token;
-    } catch (e) {
-      return e;
-    }
+    const response = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      qs.stringify({
+        grant_type: "authorization_code",
+        client_id: import.meta.env.VITE_KAKAO_CLIENT_ID,
+        redirect_uri: import.meta.env.DEV
+          ? "http://127.0.0.1:5173/oauth/kakao"
+          : import.meta.env.VITE_KAKAO_REDIRECT_URI,
+        code: authorizationCode,
+        client_secret: import.meta.env.VITE_KAKAO_CLIENT_SECRET,
+      }),
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      },
+    );
+    return response.data.access_token;
   };
 
   const handlePostKakaoLogin = async (accessToken: string) => {
     const data = await loginApi.postSocialLogin("kakao", accessToken, "", "");
-    alert("post login" + data);
-    alert("post login" + data.data.newMember);
 
     data.data.newMember
       ? navigate(`${routePaths.Join_}${routePaths.Join_Agree}`, { state: { socialLoginToken: data.data.accessToken } })
       : loginWithUserToken(data.data.accessToken);
-    alert("navigate 혹은 login 요청 보냄");
   };
 
   useEffect(() => {
